cfe263346ff3f62f660a32a0f5608a17325ea01d
Alice Quintanilla <quintana@southwestern.edu>
2017-06-07 16:48:32 -0500

    first update

---
+++ SUai/NNEvaluationFunction.java
@@ -0,0 +1,47 @@
+package SUai;
+
+import ai.evaluation.EvaluationFunction;
+import rts.GameState;
+import rts.PhysicalGameState;
+
+/**
+ * 
+ * @author alicequint
+ *
+ * @param NN
+ */
+public abstract class NNEvaluationFunction<T extends Network> extends EvaluationFunction{
+
+	protected Network nn;
+	protected PhysicalGameState pgs;
+	
+	public NNEvaluationFunction(){
+	}
+	
+	public void setNetwork(Genotype<T> g) {
+		nn = g.getPhenotype();
+	}
+	
+	/**
+	 *  creates the array to be given to the NN
+	 */
+	protected abstract double[] gameStateToArray(GameState gs);
+	
+	/**
+	 * 
+	 * @return labels of sensors given to nn
+	 */
+	public abstract String[] sensorLabels();
+	
+	/**
+	 * maximum possible thing returned by the evaluation function
+	 */
+	@Override
+	public float upperBound(GameState gs) {
+		return 1;
+	}
+	
+	public void givePhysicalGameState(PhysicalGameState pgs) {
+		this.pgs = pgs;
+	}
+}
